65d4d7190870041d5a6706217c22d039bb3f8ce2
Äkwav <ekwav@coflnet.com>
2022-07-10 12:00:16 +0200

    fix enchantments in key

--- Models/AuctionKey.cs
+++ Models/AuctionKey.cs
@@ -61,7 +61,7 @@ namespace Coflnet.Sky.Sniper.Models
 
         public override string ToString()
         {
-            return $"{(Enchants == null ? "nm" : string.Join(',',Enchants.Select(m=>m.ToString())))} {Reforge} {(Modifiers == null ? "nm" : string.Join(',',Modifiers.Select(m=>m.ToString())))} {Tier} {Count}";
+            return $"{(Enchants == null ? "nm" : string.Join(',',Enchants.Select(m=>$"{m.Type}={m.Lvl}")))} {Reforge} {(Modifiers == null ? "nm" : string.Join(',',Modifiers.Select(m=>m.ToString())))} {Tier} {Count}";
         }
 
         public AuctionKey(List<Enchantment> enchants, ItemReferences.Reforge reforge, List<KeyValuePair<string, string>> modifiers, Tier tier, byte count)
