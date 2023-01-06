eba7e0290f4d3b4d533642b9395336f30946589e
Äkwav <ekwav@coflnet.com>
2023-01-06 17:36:45 +0100

    increase comparison share

--- Services/SniperService.cs
+++ Services/SniperService.cs
@@ -660,7 +660,7 @@ ORDER BY l.`AuctionId`  DESC;
 
                 if (!l.TryGetValue(key, out ReferenceAuctions bucket))
                 {
-                    if (triggerEvents && i == 4 && Random.Shared.NextDouble() < 0.15)
+                    if (triggerEvents && i == 4 && Random.Shared.NextDouble() < 0.35)
                     {
                         Console.WriteLine($"could not find bucket {key} for {auction.Tag} {l.Count} {auction.Uuid}");
                         var closests = FindClosest(l, key).Take(5).ToList();
