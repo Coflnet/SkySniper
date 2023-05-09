e543a76f1978d6bfb4d6893c0ca547262b8e0d8a
Äkwav <ekwav@coflnet.com>
2023-05-09 23:34:00 +0200

    refactor Enchantment class

--- Models/Enchantment.cs
+++ Models/Enchantment.cs
@@ -1,31 +1,28 @@
 using MessagePack;
 
-namespace Coflnet.Sky.Sniper.Models
+namespace Coflnet.Sky.Sniper.Models;
+[MessagePackObject]
+public class Enchantment
 {
-    [MessagePackObject]
-    public class Enchantment
+    [Key(0)]
+    public Core.Enchantment.EnchantmentType Type;
+    [Key(1)]
+    public byte Lvl;
+
+    public override bool Equals(object obj)
     {
-        [Key(0)]
-        public Core.Enchantment.EnchantmentType Type;
-        [Key(1)]
-        public byte Lvl;
-
-        public override bool Equals(object obj)
-        {
-            return obj is Enchantment ench
-                && ench.Lvl == Lvl
-                && ench.Type == Type;
-        }
-
-        public override int GetHashCode()
-        {
-            return (int)Type << 4 + Lvl;
-        }
-
-        public override string ToString()
-        {
-            return base.ToString();
-        }
+        return obj is Enchantment ench
+            && ench.Lvl == Lvl
+            && ench.Type == Type;
     }
 
+    public override int GetHashCode()
+    {
+        return (int)Type << 4 + Lvl;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
 }
