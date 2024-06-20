84e5a2c3daf44bee87385260a86441f9c56ab5ca
Äkwav <ekwav@coflnet.com>
2024-06-20 10:32:19 +0200

    hanlde not present kill count

--- Services/SniperService.cs
+++ Services/SniperService.cs
@@ -2223,7 +2223,7 @@ ORDER BY l.`AuctionId`  DESC;
                 {
                     var killCount = int.Parse(killModifier.Value);
                     var present = key.Modifiers.FirstOrDefault(n => n.Key == killModifier.Key);
-                    var difference = killCount - int.Parse(present.Value);
+                    var difference = killCount - int.Parse(present.Value ?? "0");
                     var killPrice = difference * 1_000_000;
                     if (difference < 0)
                         killPrice /= 2; // only half for adding kills
