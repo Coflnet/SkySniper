842a03447fca4888d4994deb96288ad568aa9ae4
Äkwav <ekwav@coflnet.com>
2024-11-01 11:53:24 +0100

    update client version

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,12 +1,13 @@
-VERSION=0.11.0
+VERSION=0.11.1
+PACKAGE_NAME=Coflnet.Sky.Sniper.Client
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5022/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Sky.Sniper.Client,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
+-o /local/out --additional-properties=packageName=$PACKAGE_NAME,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
 
 cd out
-path=src/Coflnet.Sky.Sniper.Client/Coflnet.Sky.Sniper.Client.csproj
+path=src/$PACKAGE_NAME/$PACKAGE_NAME.csproj
 sed -i 's/GIT_USER_ID/Coflnet/g' $path
 sed -i 's/GIT_REPO_ID/SkyApi/g' $path
 sed -i 's/>OpenAPI/>Coflnet/g' $path
@@ -14,4 +15,5 @@ sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>
 sed -i '34i    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>' $path
 
 dotnet pack
-cp src/Coflnet.Sky.Sniper.Client/bin/Release/Coflnet.Sky.Sniper.Client.*.nupkg ..
+cp src/$PACKAGE_NAME/bin/Release/$PACKAGE_NAME.*.nupkg ..
+dotnet nuget push ../$PACKAGE_NAME.$VERSION.nupkg --api-key $NUGET_API_KEY --source "nuget.org" --skip-duplicate
