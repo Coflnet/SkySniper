90e65c876eff4cb08ef6a1ce3761d66caa2ac72b
Äkwav <ekwav@coflnet.com>
2024-11-23 13:09:00 +0100

    bump candy value on skinned pet fix #245

--- Services/SniperService.cs
+++ Services/SniperService.cs
@@ -1836,7 +1836,7 @@ ORDER BY l.`AuctionId`  DESC;
                 sum += (int)(factor * (float.Parse(mod.Value) + 1));
             }
             if (mod.Key == "candyUsed")
-                sum += Math.Max(GetPriceForItem(tag) / 6, 10_000_000); // for skined pets important
+                sum += Math.Max(GetPriceForItem(tag) / 6, 10_000_000) * (flatNbt.ContainsKey("skin") ? 10 : 1); // for skined pets important
             if (mod.Key == "is_shiny")
                 sum += 88_000_000;
             if (mod.Key == "party_hat_color")
