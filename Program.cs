ccdc5c3c97d6379811bfc7a46fbf0edabc6fb7ad
Äkwav <ekwav@coflnet.com>
2025-03-26 20:55:28 +0100

    stop running tests directly

--- Program.cs
+++ Program.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Coflnet.Sky.Sniper.Services;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Coflnet.Sky.Sniper
 {
@@ -14,12 +7,8 @@ namespace Coflnet.Sky.Sniper
     {
         public static void Main(string[] args)
         {
-            var test = new DropOffTests();
-            test.Setup();
-            test.CleanValueCorrect().Wait();
-            Console.WriteLine("Tests done");
             System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
-           // CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
